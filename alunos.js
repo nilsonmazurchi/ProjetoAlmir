5d7dc91f53883ae62c343b37dd3bae895d45d4a7
nilsonmazurchi <nmazurchi@gmail.com>
2023-04-15 09:42:05 -0300

    Final

---
+++ alunos.js
@@ -0,0 +1,63 @@
+const alunos = require("./alunos.json")
+
+// const alunos = [
+//     { nome: "José Almir", matrícula: 15897968916, media: 7.0 },
+//     { nome: "José Carlos", matrícula: 15897968917, media: 6.5 },
+//     { nome: "Fernando Fernandes", matrícula: 15897968910, media: 4.5 },
+//     { nome: "Cristiano Pereira", matrícula: 15897968911, media: 5.5 },
+//     { nome: "João Cardozo", matrícula: 15897968912, media: 8.5 },
+//     { nome: "João Pedro Lima", matrícula: 15897968913, media: 2.5 },
+//     { nome: "Renata Adriana Silva", matrícula: 15897968914, media: 3.5 },
+//     { nome: "Aline Cardoso", matrícula: 15897968915, media: 10.0 },
+//     { nome: "Sofia Marques", matrícula: 15897968918, media: 6.2 },
+//     { nome: "Adriana Santos", matrícula: 15897968919, media: 3.5 },
+    
+//   ];
+
+  // Função de filtragem por nome
+  function buscaNome(alunos, nome){
+    return alunos.filter(aluno => {
+      return aluno.nome.toLowerCase().includes(nome.toLowerCase());
+    })
+
+  }
+
+  // Função de filtragem por média
+  function buscaMedia(alunos, media){
+    if (media >= 7) {
+      return alunos.filter(aluno => aluno.media >= media);
+  }
+  }
+  
+// Função para deletar um aluno da lista
+
+function deletarAluno(index) {
+  if (index < 0 || index >= alunos.length) {
+      return false;
+  } else {
+      alunos.splice(index, 1);
+      return true;
+}}
+
+// Função para atualizar os dados dos alunos dentro do array
+
+function atualizarDadosAluno(index, nome, matricula, media) {
+  
+  if (index < 0 || index >= alunos.length) {
+      throw "Aluno não encontrado";
+  } else {
+    alunos[index].nome = nome;
+    alunos[index].media = Number(media);
+    alunos[index].matricula = Number(matricula);
+    return nome, media, matricula
+  }
+  
+  
+
+}
+// // Exportação do array e das funções 
+
+  module.exports = {alunos, buscaNome, buscaMedia, deletarAluno, atualizarDadosAluno}
+  
+
+  
---
+++ index.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const {alunos, buscaNome, buscaMedia, deletarAluno, atualizarDadosAluno} = require("./alunos");
+const bodyParser = require('body-parser');
+const fs = require("fs");
+const morganBody = require('morgan-Body');
+const path = require('path');
+const morgan = require('morgan')
+//const moment = require("moment/moment");
+
+// const server = http.createServer(app)
+
+const app = express();
+app.use(bodyParser.json());
+
+// Aplicando Morgan para salvar o log em arquivo
+const log = fs.createWriteStream(
+    path.join(__dirname, "./logs", "express.log"), { flags: "a" }
+);
+  
+morganBody(app, {
+    noColors: true,
+    stream: log,
+});
+
+
+// Get para /alunos
+app.get("/alunos", morgan('combined'), (req, res) => {
+    let listaAlunos = alunos;
+
+    if (req.query.nome) {
+        listaAlunos = buscaNome(listaAlunos, req.query.nome);
+    }
+
+    if (req.query.media) {
+        listaAlunos = buscaMedia(listaAlunos, parseFloat(req.query.media));
+    }
+
+    res.json(listaAlunos);
+    })
+
+// POST para novo aluno
+
+app.post("/alunos/novo", morgan('tiny'), (req, res) => {
+    const { nome, matricula, media } = req.query;
+
+    const novoAluno = { nome: nome,  matricula: matricula, media: media };
+    alunos.push(novoAluno);
+    fs.writeFile("alunos.json", JSON.stringify(alunos), (err) => {
+        if (err) throw err;
+    })
+    
+
+    if (novoAluno) {
+        res.status(201).json({ message: "Usuário adicionado" });
+    } else {
+        res.status(400).json({ message: "Aluno não encontrado" });
+    }
+});
+
+// Deletar aluno
+
+// app.post("/alunos/deletar/:index", (req, res) => {
+//     const index = parseInt(req.params.index);
+
+//     try {
+//         deletarAluno(index);
+//         fs.writeFile("alunos.json", JSON.stringify(alunos), (err) => {
+//             if (err) throw err;
+//         })
+//         res.json({ message: "Aluno removido com sucesso" });
+//     } catch (error) {
+//         res.status(404).json({ error: error.message });
+//     }
+// });
+
+app.delete("/alunos/:index", morgan(':url :method'), (req, res) => {
+    const index = parseInt(req.params.index);
+   
+    if(alunos[index]) {
+        deletarAluno(index);
+        fs.writeFile("alunos.json", JSON.stringify(alunos), (err) => {
+            if (err) throw err;
+        })
+        res.json({ message: "Aluno removido com sucesso" });
+    } else  {
+        res.status(404).json({ error: "O index não existe" });
+    }
+});
+
+
+
+// Atualizar a lista de alunos
+
+// app.post("/alunos/atualizar/:index", (req, res) => {
+//     const index = req.params.index;
+//     const { nome, matricula, media } = req.query;
+
+//     if (!alunos[index]) {
+//         res.status(404).json({ error: 'Aluno não encontrado' });
+//         return;
+//     }else {
+//         atualizarDadosAluno(index, nome, matricula, media);
+//         fs.writeFile("alunos.json", JSON.stringify(alunos), (err) => {
+//             if (err) throw err;
+//         })
+//     }
+//     res.json(alunos);
+// })
+
+
+
+app.put("/alunos/:index", (req, res) => {
+    const index = (req.params.index);
+    const { nome, matricula, media } = req.query;
+    
+    if (!alunos[index]) {
+        res.status(404).json({ error: 'Aluno não encontrado' });
+        return;
+    }else {
+        atualizarDadosAluno(index, nome, matricula, media);
+        fs.writeFile("alunos.json", JSON.stringify(alunos), (err) => {
+            if (err) throw err;
+        })
+    }
+    res.json(alunos);
+})
+
+
+
+// Escuta
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000/");
+});
+
+
+
